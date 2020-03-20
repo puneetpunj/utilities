633ddd3798a6ef88f9ce4f5bd487b5cb5a5c9a31
Puneet Punj <puneetpunj88@gmail.com>
2020-03-20 19:39:45 +1100

    updated logic for file comparison

--- csvcomparison.js
+++ csvcomparison.js
@@ -1,37 +1,31 @@
 const fs = require('fs');
 const generateDeltaFile = (pathToBaseFile, pathToFileForComparison, pathForOutputFileName = './difference.csv') => {
-    let internFile = "", externFile = '', changedLine = ''
+    let baseFileContent = '', secondaryFileContent = '', changedLine = ''
     return new Promise((resolve, reject) => {
-        const rs = fs.createReadStream(pathToBaseFile)
-        rs
-            .on('data', function (chunk) {
-                internFile = internFile + chunk
-            })
-            .on('end', function () {
-                const rs1 = fs.createReadStream(pathToFileForComparison)
+        const baseFileReadstream = fs.createReadStream(pathToBaseFile)
+        baseFileReadstream
+            .on('data', chunk => { baseFileContent = baseFileContent + chunk })
+            .on('error', (err) => reject(err))
+            .on('end', () => {
+                const secondaryFileReadstream = fs.createReadStream(pathToFileForComparison)
+                secondaryFileReadstream
+                    .on('data', data => { secondaryFileContent = secondaryFileContent + data })
+                    .on('error', (err) => reject(err))
+                    .on('end', () => {
+                        // split all lines by \n to form an array for both base and secondary files
+                        const internLines = baseFileContent.toString().split('\n');
+                        const externLines = secondaryFileContent.toString().split('\n');
 
-                rs1
-                    .on('data', function (data) {
-                        externFile = externFile + data
-                    })
-                    .on('end', function () {
-                        let internLines = internFile.toString().split('\n');
-                        let externLines = externFile.toString().split('\n');
+                        // Create a json object with each secondary file line as its key and value as true
+                        const externLookup = {};
+                        externLines.forEach(eLine => externLookup[eLine] = true);
 
-                        let externLookup = {};   //Added line
-                        externLines.forEach(function (eLine) {   //Added line
-
-                            externLookup[eLine.replace('\r', '')] = true;         //Added line
-                        });
-
-                        internLines.forEach(function (iLine) {
-                            let internCells
-
-                            internCells = iLine;
-
-                            if (!externLookup[internCells]) {  //Changed line
-                                changedLine = changedLine + iLine + '\n'
-                            }
+                        // Iterate through each line of base file
+                        internLines.forEach(iLine => {
+                            // use above formed json object and pass each line as key
+                            // value of externLookup[iLine] would be undefined if secondary file didn't have same line
+                            // in that case current line is considered as changed line and will be eventually written to output file
+                            if (!externLookup[iLine]) changedLine = changedLine + iLine + '\n'
                         })
 
                         fs.writeFileSync(pathForOutputFileName, changedLine)
@@ -42,5 +36,7 @@ const generateDeltaFile = (pathToBaseFile, pathToFileForComparison, pathForOutpu
 }
 
 (async () => {
-    console.log(await generateDeltaFile('./base.csv', './comparisonAgainst.csv'))
+    console.time("csv comparison time taken");
+    console.log(await generateDeltaFile('./base-file.csv', './secondary-file.csv'))
+    console.timeEnd("csv comparison time taken")
 })()
